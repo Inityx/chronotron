03009978fcfad02198106cf69b184a7cfc4650ea
Andrew Tolvstad <Andrew@localhost>
2017-12-04 05:24:59 -0800

    Use const and fix CSS

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
 "use strict";
 
-var express = require('express');
-var body_parser = require('body-parser');
-var app = express();
+const express = require('express');
+const body_parser = require('body-parser');
+const app = express();
 
 app.use(body_parser.urlencoded({extended: false}));
 app.use(body_parser.json());
 app.use(express.static('static'))
 
-var PORT = 8000;
-var TIMELINES = require('./timelines.json');
+const PORT = 8000;
+const TIMELINES = require('./timelines.json');
 
 function dig(obj, trail) {
     if (trail.length == 0) return obj;
 
-    let key = trail.shift();
-    let value = obj[key];
+    const key = trail.shift();
+    const value = obj[key];
     
     if (!value) return value;
     return dig(value, trail);
 }
 
 app.post('/query/', (request, response) => {
-    let keys = JSON.parse(request.body.keys);
-    let value = dig(TIMELINES, keys);
+    const keys = JSON.parse(request.body.keys);
+    const value = dig(TIMELINES, keys);
 
     if (!value) {
         response.status = 404;
         response.send('Not found.');
     }
     
-    let response_data;
-    if (Array.isArray(value))
-        // Value is a timeline
-        response_data = value;
-    else
-        // Value is a directory
-        response_data = Object.keys(value);
+    const response_data = Array.isArray(value) ?
+        value :             // Value is a timeline
+        Object.keys(value); // Value is a directory
 
     res.send(JSON.stringify(response_data));
 });
